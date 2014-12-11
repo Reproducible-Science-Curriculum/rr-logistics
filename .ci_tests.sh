912ece8ab4a9dd2971ecc599144414d49f7b53e4
Rich FitzJohn <rich.fitzjohn@gmail.com>
2014-12-11 13:20:01 -0500

    Move the scripting to R
    Easier to assume R knowledge than shell knowlege.  More tools available,
    increases chance that the boilerplate will be able to stay the same.

--- .ci_tests.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh -x
-# The error setting code here means that knitr will fail if there is
-# an error in the document.  This is a tricky line to walk if you
-# *want* to capture errors in your document, but that is probably an
-# edge case.
-Rscript -e "library(knitr); knitr::opts_chunk$set(error=FALSE); knit('report.Rmd')"
