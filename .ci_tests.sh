7bf5f440615dad4f46110df1f67714263d61d1db
Rich FitzJohn <rich.fitzjohn@gmail.com>
2014-12-11 13:05:47 -0500

    Fail if there is a knitr error

--- .ci_tests.sh
+++ .ci_tests.sh
@@ -1,2 +1,6 @@
 #!/bin/sh -x
-Rscript -e "library(knitr); knit('report.Rmd')"
+# The error setting code here means that knitr will fail if there is
+# an error in the document.  This is a tricky line to walk if you
+# *want* to capture errors in your document, but that is probably an
+# edge case.
+Rscript -e "library(knitr); knitr::opts_chunk$set(error=FALSE); knit('report.Rmd')"
